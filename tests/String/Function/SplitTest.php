24177898c0cf4cd93040a4c23a8e2745aa598f96
ReenExe <ipourri@gmail.com>
2013-12-03 19:11:03 +0200

    progress

--- tests/String/Function/SplitTest.php
+++ tests/String/Function/SplitTest.php
@@ -13,6 +13,29 @@ class SplitTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(str_split($string, $length), $expected);
     }
 
+    public function testDefaultLength()
+    {
+        $this->assertSame(str_split('abc'), ['a', 'b', 'c']);
+    }
+
+    /**
+     * @dataProvider failLengthDataProvider
+     * @expectedException \PHPUnit_Framework_Error
+     * @expectedExceptionMessage str_split(): The length of each segment must be greater than zero
+     * @param int $length
+     */
+    public function testFailLength(int $length)
+    {
+        str_split('abc', $length);
+    }
+
+    public function failLengthDataProvider()
+    {
+        for ($i = -3; $i < 0; ++$i) {
+            yield [$i];
+        }
+    }
+
     public function dataProvider()
     {
         yield [
