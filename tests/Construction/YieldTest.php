62fe51509a138ffa58e7ebc3f8bf3805fb3b0fa0
ReenExe <ipourri@gmail.com>
2015-05-09 20:25:54 +0300

    test yield with return priority

--- tests/Construction/YieldTest.php
+++ tests/Construction/YieldTest.php
@@ -12,4 +12,36 @@ class YieldTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($value, 100);
     }
+
+    public function testPriority()
+    {
+        /**
+         * Такое запрещено делать
+         * Результат:
+         *  `Fatal error: Generators cannot return values using "return"`
+         */
+
+        /**
+            1.
+                $both = function() {
+                    yield 1;
+                    return [$generatorBehavior];
+                };
+
+                foreach ($both() as $value) {}
+         */
+
+        /**
+            2.
+                $both = function($generatorBehavior = true) {
+                    if ($generatorBehavior) {
+                        yield 1;
+                    } else {
+                        return [$generatorBehavior];
+                    }
+                };
+
+                foreach ($both() as $value) {}
+         */
+    }
 }
