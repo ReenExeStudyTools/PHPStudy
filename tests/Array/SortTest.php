e64e0e97c8eb3775c5cc5899c650ce72ba1f8acc
ReenExe <ipourri@gmail.com>
2015-05-14 00:10:40 +0300

    assoc sort test

--- tests/Array/SortTest.php
+++ tests/Array/SortTest.php
@@ -26,4 +26,45 @@ class SortTest extends \PHPUnit_Framework_TestCase
             ],
         ];
     }
+
+    /**
+     * @dataProvider assocSortProvider
+     */
+    public function testAssocSort(array $array, array $expect, array $reverse)
+    {
+        asort($array);
+        $this->assertSame($array, $expect);
+
+        arsort($array);
+        $this->assertSame($array, $reverse);
+    }
+
+    public function assocSortProvider()
+    {
+        return [
+            [
+                [
+                    3,
+                    'y' => 5,
+                    'x' => 1,
+                    7,
+                ],
+
+                [
+                    'x' => 1,
+                    3,
+                    'y' => 5,
+                    7,
+                ],
+
+                [
+                    1 => 7,
+                    'y' => 5,
+                    0 => 3,
+                    'x' => 1,
+                ],
+
+            ],
+        ];
+    }
 }
