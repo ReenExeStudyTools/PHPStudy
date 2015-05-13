3bdf740a9ad42f098ad09d02631cc0c533a03217
ReenExe <ipourri@gmail.com>
2015-05-13 22:59:26 +0300

    double link

--- tests/Basics/ReferenceTest.php
+++ tests/Basics/ReferenceTest.php
@@ -6,7 +6,13 @@ class ReferenceTest extends \PHPUnit_Framework_TestCase
     {
         $a = 1;
         $b = &$a;
+
         $b = 2;
         $this->assertSame($a, 2);
+
+        $c = &$b;
+
+        $c = 3;
+        $this->assertSame($a, 3);
     }
 }
