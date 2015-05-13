a14b7d2d759d74220fe8ddee719745d29a78bc11
ReenExe <ipourri@gmail.com>
2015-05-13 23:17:12 +0300

    multi reference test

--- tests/Basics/ReferenceTest.php
+++ tests/Basics/ReferenceTest.php
@@ -14,5 +14,20 @@ class ReferenceTest extends \PHPUnit_Framework_TestCase
 
         $c = 3;
         $this->assertSame($a, 3);
+
+        /**
+         * Цікаво ж, що буде?
+         */
+        list($a, $b, $c) = [5, 6, 7];
+
+        /**
+         * Припущення - що всі значення будуть однакові і рівні останньому, а саме 7
+         */
+        $this->assertSame($a, 5);
+        $this->assertSame($b, 5);
+        $this->assertSame($c, 5);
+        /**
+         * Але результати дивують
+         */
     }
 }
