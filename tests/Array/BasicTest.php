6a598cbbb9b93a0202157837700d7cef7ac54edc
ReenExe <ipourri@gmail.com>
2015-07-08 01:37:54 +0300

    from question

--- tests/Array/BasicTest.php
+++ tests/Array/BasicTest.php
@@ -15,4 +15,13 @@ class BasicTest extends \PHPUnit_Framework_TestCase
 
         $this->assertSame($array[8], 'next');
     }
+
+    public function testNumeration()
+    {
+        $array = [
+            '1' => 'One', 1 => 'Two', 'Three', 2 => 'Four'
+        ];
+
+        $this->assertTrue($array === [1 => 'Two', 2 => 'Four']);
+    }
 }
