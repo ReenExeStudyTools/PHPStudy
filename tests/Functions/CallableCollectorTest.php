00e3243d210c67b4b00294b69eb45b6c82020659
ReenExe <ipourri@gmail.com>
2015-07-09 23:48:24 +0300

    progress

--- tests/Functions/CallableCollectorTest.php
+++ tests/Functions/CallableCollectorTest.php
@@ -7,5 +7,11 @@ class CallableCollectorTest extends \PHPUnit_Framework_TestCase
         $collector = new \ReenExe\Study\CallableCollector(1, function($l, $r) {
             return $l + $r;
         });
+
+        foreach ([2, 3, 4, 5, 6] as $value) {
+            $collector($value);
+        }
+
+        $this->assertEquals($collector->getResult(), 21);
     }
 }
