5071bdb1948fced6af579e558e4867172a1cc187
ReenExe <ipourri@gmail.com>
2015-07-25 01:24:45 +0300

    progress XOR

--- tests/Oparation/BinaryTest.php
+++ tests/Oparation/BinaryTest.php
@@ -42,4 +42,33 @@ class BinaryTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue((1 & 2 & 3 & 4) === 0b0);
         $this->assertTrue((0b1 & 0b10 & 0b11 & 0b100) === 0b0);
     }
+
+    public function testXOR()
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            // simple
+            $this->assertTrue(($i ^ $i) === 0);
+
+            // recovery
+
+            for ($j = 0; $j < 10; ++$j) {
+                $this->assertTrue(($i ^ $j ^ $j) === $i);
+            }
+        }
+
+        $this->assertTrue((1 ^ 0) === 1);
+        $this->assertTrue((0b1 ^ 0b0) === 0b1);
+
+        $this->assertTrue((1 ^ 2) === 3);
+        $this->assertTrue((0b1 ^ 0b10) === 0b11);
+
+        $this->assertTrue((1 ^ 3) === 2);
+        $this->assertTrue((0b01 ^ 0b11) === 0b10);
+
+        $this->assertTrue((1 ^ 2 ^ 4) === 7);
+        $this->assertTrue((0b1 ^ 0b10 ^ 0b100) === 0b111);
+
+        $this->assertTrue((1 ^ 2 ^ 3 ^ 4) === 0b100);
+        $this->assertTrue((0b1 ^ 0b10 ^ 0b11 ^ 0b100) === 0b100);
+    }
 }
