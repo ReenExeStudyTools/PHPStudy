4e5b3bfd83dd0990d3012b2b6c959d58a59d8ff0
ReenExe <ipourri@gmail.com>
2015-07-25 01:36:03 +0300

    constant

---
+++ tests/Basic/ConstantTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class ConstantTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \PHPUnit_Framework_Error_Warning
+     */
+    public function testStrictCase()
+    {
+        define('SOME_CASE_CONSTANT', 1);
+        $this->assertTrue(constant('SOME_CASE_CONSTANT') === 1);
+        constant('some_case_constant');
+    }
+
+    public function testMultiCase()
+    {
+        define('some_multi_CASE_CONSTANT', 3, true);
+        $this->assertTrue(constant('SOME_MULTI_CASE_CONSTANT') === 3);
+        $this->assertTrue(constant('some_multi_case_constant') === 3);
+    }
+}
