d1a44e16719ed43108ccc5fecc5685ee86408cf7
ReenExe <ipourri@gmail.com>
2015-07-25 01:18:50 +0300

    AND progress

--- tests/Oparation/BinaryTest.php
+++ tests/Oparation/BinaryTest.php
@@ -9,15 +9,37 @@ class BinaryTest extends \PHPUnit_Framework_TestCase
         }
 
         $this->assertTrue((1 | 0) === 1);
-        $this->assertTrue((0b1 | 0b1) === 1);
+        $this->assertTrue((0b1 | 0b0) === 0b1);
 
         $this->assertTrue((1 | 2) === 3);
-        $this->assertTrue((0b1 | 0b11) === 0b11);
+        $this->assertTrue((0b1 | 0b10) === 0b11);
 
         $this->assertTrue((1 | 2 | 4) === 7);
         $this->assertTrue((0b1 | 0b10 | 0b100) === 0b111);
 
         $this->assertTrue((1 | 2 | 3 | 4) === 7);
         $this->assertTrue((0b1 | 0b10 | 0b11 | 0b100) === 0b111);
+    }
+
+    public function testAND()
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $this->assertTrue(($i & $i) === $i);
+        }
+
+        $this->assertTrue((1 & 0) === 0);
+        $this->assertTrue((0b1 & 0b0) === 0b0);
+
+        $this->assertTrue((1 & 2) === 0);
+        $this->assertTrue((0b1 & 0b10) === 0b0);
+
+        $this->assertTrue((1 & 3) === 1);
+        $this->assertTrue((0b1 & 0b11) === 0b1);
+
+        $this->assertTrue((1 & 2 & 4) === 0);
+        $this->assertTrue((0b1 & 0b10 & 0b100) === 0b0);
+
+        $this->assertTrue((1 & 2 & 3 & 4) === 0b0);
+        $this->assertTrue((0b1 & 0b10 & 0b11 & 0b100) === 0b0);
     }
 }
