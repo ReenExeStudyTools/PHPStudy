b3a7ea553df3946c269b28168ec7db0c64d0e3b5
ReenExe <ipourri@gmail.com>
2015-07-28 22:00:11 +0300

    flush keys

--- tests/Array/FunctionTest.php
+++ tests/Array/FunctionTest.php
@@ -2,23 +2,13 @@
 
 class FunctionTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @dataProvider mergeProvider
-     */
-    public function testMerge(array $arrayOfArray, array $expected)
+    public function testMerge()
     {
-        $this->assertTrue(call_user_func_array('array_merge', $arrayOfArray) === $expected);
-    }
-
-    public function mergeProvider()
-    {
-        return [
-            [
-                [
-                    [1], [2], [3]
-                ],
-                [1, 2, 3]
-            ]
-        ];
+        $this->assertTrue(array_merge([1], [2]) === [1, 2]);
+        $this->assertTrue(array_merge(['x'], ['y'], ['z']) === ['x', 'y', 'z']);
+        // flush keys
+        $this->assertTrue(
+            array_merge([1 => 'a'], [2 => 'b'], [3 => 'c']) === ['a', 'b', 'c']
+        );
     }
 }
