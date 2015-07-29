50caa74bae803279dd003fd22ff030ccc2a15e68
ReenExe <ipourri@gmail.com>
2015-07-29 22:00:17 +0300

    stay assoc

--- tests/Array/FunctionTest.php
+++ tests/Array/FunctionTest.php
@@ -4,9 +4,15 @@ class FunctionTest extends \PHPUnit_Framework_TestCase
 {
     public function testMerge()
     {
+        // can merge one params
+        $this->assertTrue(array_merge([1]) === [1]);
+        // flush numeric keys
+        $this->assertTrue(array_merge([1 => 1]) === [1]);
+        // stay assoc keys
+        $this->assertTrue(array_merge(['k' => 'v']) === ['k' => 'v']);
         $this->assertTrue(array_merge([1], [2]) === [1, 2]);
         $this->assertTrue(array_merge(['x'], ['y'], ['z']) === ['x', 'y', 'z']);
-        // flush keys
+        // flush numeric keys
         $this->assertTrue(
             array_merge([1 => 'a'], [2 => 'b'], [3 => 'c']) === ['a', 'b', 'c']
         );
