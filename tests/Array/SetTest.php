2c1c3fd6981cf80596ec1ac83cf9b5e37b8cae8d
ReenExe <ipourri@gmail.com>
2015-08-05 00:41:08 +0300

    SORT_NUMERIC

--- tests/Array/SetTest.php
+++ tests/Array/SetTest.php
@@ -3,17 +3,17 @@
 class SetTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @dataProvider uniqueProvider
+     * @dataProvider uniqueProviderSortString
      * @param array $source
      * @param array $expected
      */
-    public function testUnique(array $source, array $expected)
+    public function testUniqueSortString(array $source, array $expected)
     {
         $this->assertTrue(array_unique($source) === $expected);
         $this->assertTrue(array_unique($source, SORT_STRING) === $expected);
     }
 
-    public function uniqueProvider()
+    public function uniqueProviderSortString()
     {
         return [
             [
@@ -23,6 +23,10 @@ class SetTest extends \PHPUnit_Framework_TestCase
             [
                 [1 => 1, 1, 1],
                 [1 => 1]
+            ],
+            [
+                [1, 1, 1, 'value'],
+                [1, 3 => 'value']
             ],
             [
                 ['a' => 1, 1, 1],
@@ -65,6 +69,34 @@ class SetTest extends \PHPUnit_Framework_TestCase
                 [1, '1', 2 => '2', 2, 4 => 3, '3'],
                 [1, 2 => '2', 4 => 3]
             ]
+        ];
+    }
+
+    /**
+     * @dataProvider uniqueProviderSortNumeric
+     * @param array $source
+     * @param array $expected
+     */
+    public function testUniqueSortNumeric(array $source, array $expected)
+    {
+        $this->assertTrue(array_unique($source, SORT_NUMERIC) === $expected);
+    }
+
+    public function uniqueProviderSortNumeric()
+    {
+        return [
+            [
+                [1, '1', '1value', 'value'],
+                [1, 3 => 'value']
+            ],
+            [
+                ['1value', '1key'],
+                ['1value']
+            ],
+            [
+                ['1value', 1],
+                ['1value']
+            ],
         ];
     }
 }
