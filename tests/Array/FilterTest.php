82d179bf17eda8665c279b8b9cc154ddf9c3852f
ReenExe <ipourri@gmail.com>
2015-08-11 22:00:15 +0300

    array_filter progress

--- tests/Array/FilterTest.php
+++ tests/Array/FilterTest.php
@@ -8,6 +8,7 @@ class FilterTest extends \PHPUnit_Framework_TestCase
     public function test(array $source, array $expected)
     {
         $this->assertTrue(array_filter($source) === $expected);
+        $this->assertTrue(array_filter($source, 'boolval') === $expected);
     }
 
     public function dataProvider()
@@ -26,5 +27,26 @@ class FilterTest extends \PHPUnit_Framework_TestCase
                 [false, 1 => 1], [1 => 1]
             ],
         ];
+    }
+
+    /**
+     * @dataProvider callbackDataProvider
+     */
+    public function testCallback(array $source, array $expected, $callback)
+    {
+        $this->assertTrue(array_filter($source, $callback) === $expected);
+    }
+
+    public function callbackDataProvider()
+    {
+        $list = ['intval', 'is_int', 'is_numeric'];
+
+        foreach ($list as $callback) {
+            yield [
+                ['a', 'b', 'c', 1, 2, 3],
+                [3 => 1, 4 => 2, 5 => 3],
+                $callback
+            ];
+        }
     }
 }
