091b4b4d739a29c1f2fbe7b36b62f0a326c4b4db
ReenExe <ipourri@gmail.com>
2015-08-15 23:50:01 +0300

    array_diff progress

--- tests/Array/SetTest.php
+++ tests/Array/SetTest.php
@@ -109,11 +109,28 @@ class SetTest extends \PHPUnit_Framework_TestCase
 
     public function testDiff()
     {
+        /**
+         * Note: Two elements are considered equal
+         * if and only if (string) $elem1 === (string) $elem2.
+         * In words: when the string representation is the same.
+         */
+
         $this->assertTrue(array_diff([], []) === []);
         $this->assertTrue(array_diff([1], []) === [1]);
         $this->assertTrue(array_diff(['a' => 1], []) === ['a' => 1]);
         $this->assertTrue(array_diff([1 => 1], []) === [1 => 1]);
         $this->assertTrue(array_diff([1, 2, 3], []) === [1, 2, 3]);
+
+        $this->assertTrue(array_diff(['a', 'b', 'c'], ['d'], ['e']) === ['a', 'b', 'c']);
+        $this->assertTrue(array_diff(['a', 'b', 'c'], ['a'], ['b'], ['c']) === []);
+        $this->assertTrue(array_diff(['a', 'b', 'c'], ['a', 'b'], ['c']) === []);
+        $this->assertTrue(array_diff(['a', 'b', 'c'], ['a', 'b']) === [2 => 'c']);
+
+        $this->assertTrue(array_diff(['1', true], [1]) === []);
+        $this->assertTrue(array_diff([0, null, '', false, '0'], ['']) === [0, 4 => '0']);
+        $this->assertTrue(
+            array_diff([0, null, '', false, '0'], [0]) === [1 => null, 2 => '', 3 => false]
+        );
     }
     /**
      * TODO: SORT_REGULAR
