4d64eb93054ba7f8d5b268931d99ec370833cbcc
ReenExe <ipourri@gmail.com>
2015-09-01 22:03:02 +0300

    array_map progress

--- tests/Array/FunctionTest.php
+++ tests/Array/FunctionTest.php
@@ -263,5 +263,6 @@ class FunctionTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertTrue(array_map('intval', ['1', '2']) === [1, 2]);
         $this->assertTrue(array_map('max', [5, 1], [3, 2]) === [5, 2]);
+        $this->assertTrue(array_map('max', [5, 1, 3], [3, 2]) === [5, 2, 3]);
     }
 }
