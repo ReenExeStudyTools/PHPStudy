99c0579b6233504a9a9b532d4d29279690b8932d
ReenExe <ipourri@gmail.com>
2015-09-27 11:50:07 +0300

    progress

--- tests/Basic/VariadicTest.php
+++ tests/Basic/VariadicTest.php
@@ -2,11 +2,22 @@
 
 class VariadicTest extends \PHPUnit_Framework_TestCase
 {
-    public function test()
+    public function testPush()
     {
         $array = ['a'];
         $push = ['b', 'c'];
         array_push($array, ... $push);
         $this->assertSame($array, ['a', 'b', 'c']);
+    }
+
+    public function testMerge()
+    {
+        $array = [
+            [1],
+            [2],
+            [3],
+        ];
+
+        $this->assertSame(array_merge(...$array), [1, 2, 3]);
     }
 }
