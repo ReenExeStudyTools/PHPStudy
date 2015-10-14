c3728c0ee59bc23934bcebc663c3b4bb1a85be73
ReenExe <ipourri@gmail.com>
2015-10-14 21:57:51 +0300

    progress

--- tests/Reflection/ReflectionFunctionTest.php
+++ tests/Reflection/ReflectionFunctionTest.php
@@ -10,6 +10,11 @@ class ReflectionFunctionTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($reflection->getShortName(), 'intval');
         $this->assertSame($reflection->name, 'intval');
 
+        /* @var $parameters ReflectionParameter[] */
+        $parameters = $reflection->getParameters();
+        $this->assertTrue(is_array($parameters));
+
+        $this->assertSame(count($parameters), 2);
         $this->assertSame($reflection->getNumberOfParameters(), 2);
         $this->assertSame($reflection->getNumberOfRequiredParameters(), 1);
 
@@ -17,5 +22,15 @@ class ReflectionFunctionTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFalse($reflection->getDocComment());
         $this->assertFalse($reflection->getFileName());
+
+        /* @var $varParam ReflectionParameter */
+        $varParam = $parameters[0];
+        $this->assertSame($varParam->getName(), 'var');
+        $this->assertFalse($varParam->isOptional());
+
+        /* @var $baseParam ReflectionParameter */
+        $baseParam = $parameters[1];
+        $this->assertSame($baseParam->getName(), 'base');
+        $this->assertTrue($baseParam->isOptional());
     }
 }
