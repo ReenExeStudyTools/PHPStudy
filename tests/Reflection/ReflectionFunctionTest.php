2feb76f387f9f1bb0c03233ee9379bf4b822c948
ReenExe <ipourri@gmail.com>
2015-10-14 21:52:32 +0300

    progress

--- tests/Reflection/ReflectionFunctionTest.php
+++ tests/Reflection/ReflectionFunctionTest.php
@@ -7,7 +7,15 @@ class ReflectionFunctionTest extends \PHPUnit_Framework_TestCase
     {
         $reflection = new \ReflectionFunction('intval');
         $this->assertSame($reflection->getName(), 'intval');
+        $this->assertSame($reflection->getShortName(), 'intval');
+        $this->assertSame($reflection->name, 'intval');
 
         $this->assertSame($reflection->getNumberOfParameters(), 2);
+        $this->assertSame($reflection->getNumberOfRequiredParameters(), 1);
+
+        $this->assertFalse($reflection->returnsReference());
+
+        $this->assertFalse($reflection->getDocComment());
+        $this->assertFalse($reflection->getFileName());
     }
 }
