610c17171474ba41761605a15ba79246d4d883b2
ReenExe <ipourri@gmail.com>
2015-11-06 20:01:59 +0200

    When you try to implement an interface by magic - still get: `Fatal Error`

---
+++ fixtures/classes/OOP/Magic/CallImplementInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReenExe\Study\OOP\Magic;
+
+class CallImplementInterface implements \Countable
+{
+    public function __call($name, array $args)
+    {
+
+    }
+}
---
+++ tests/OOP/Magic/CallImplementInterfaceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use ReenExe\Study\OOP\Magic\CallImplementInterface;
+
+class CallImplementInterfaceTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        /**
+         * Fatal Error: CallImplementInterface contains 1 abstract method...
+
+            new CallImplementInterface();
+
+         */
+    }
+}
