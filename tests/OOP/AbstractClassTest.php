0bf1a801f10cb775d96ef0bedb77ce9af0630fa1
ReenExe <ipourri@gmail.com>
2015-11-18 01:12:41 +0200

    Update AbstractClassTest.php

--- tests/OOP/AbstractClassTest.php
+++ tests/OOP/AbstractClassTest.php
@@ -6,6 +6,11 @@ class AbstractClassTest extends \PHPUnit_Framework_TestCase
 {
     public function test()
     {
+        try {
+            new AbstractClass();
+        } catch (\Trowable $e) {
+            
+        }
         /**
             Fatal Error
             new AbstractClass();
