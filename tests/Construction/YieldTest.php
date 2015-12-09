6bd1118efe04b172651b3f9a260a2ad238a635f8
ReenExe <ipourri@gmail.com>
2015-12-10 01:05:24 +0200

    can use mixed `return` and `yield`

--- tests/Construction/YieldTest.php
+++ tests/Construction/YieldTest.php
@@ -15,34 +15,28 @@ class YieldTest extends \PHPUnit_Framework_TestCase
 
     public function testPriority()
     {
-        /**
-         * Такое запрещено делать
-         * Результат:
-         *  `Fatal error: Generators cannot return values using "return"`
+        $both = function () {
+            yield 1;
+            return [1];
+        };
+        foreach ($both() as $value) {
+        }
 
-            1.
-                $both = function() {
-                    yield 1;
-                    return [1];
-                };
+        $both = function () {
+            return [1];
+            yield 1;
+        };
+        foreach ($both() as $value) {
+        }
 
-            2.
-                $both = function() {
-                    return [1];
-                    yield 1;
-                };
-
-            3.
-                $both = function($generatorBehavior = true) {
-                    if ($generatorBehavior) {
-                        yield 1;
-                    } else {
-                        return [1];
-                    }
-                };
-
-            Action:
-                foreach ($both() as $value) {}
-         */
+        $both = function ($generatorBehavior = true) {
+            if ($generatorBehavior) {
+                yield 1;
+            } else {
+                return [1];
+            }
+        };
+        foreach ($both() as $value) {
+        }
     }
 }
