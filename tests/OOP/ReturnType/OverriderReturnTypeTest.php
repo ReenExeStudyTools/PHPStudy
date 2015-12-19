154e5077d6b7bb31fa097ed4a6c4ceddb167b2dd
ReenExe <ipourri@gmail.com>
2015-12-19 21:30:52 +0200

    Try override method with returntype subclass

--- tests/OOP/ReturnType/OverriderReturnTypeTest.php
+++ tests/OOP/ReturnType/OverriderReturnTypeTest.php
@@ -1,9 +1,18 @@
 <?php
 
+use ReenExe\Study\OOP\ReturnType\ResponsibleEntityFactory;
+
 class OverriderReturnTypeTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Try override method with returntype subclass
+     */
     public function test()
     {
-
+        /**
+         * ERROR:
+         * Fatal error:  Declaration of ReenExe\Study\OOP\ReturnType\ResponsibleEntityFactory::create(): ReenExe\Study\OOP\ReturnType\ResponsibleEntity must be compatible with ReenExe\Study\OOP\ReturnType\EntityFactory::create(): ReenExe\Study\OOP\ReturnType\Entity
+            ResponsibleEntityFactory::create();
+         */
     }
 }
