05b96b664fb002683c5b191d2d26884c4dfd3f78
ReenExe <ipourri@gmail.com>
2016-02-21 17:21:29 +0200

    progress

--- tests/OOP/StandardInterface/ArrayAccessTest.php
+++ tests/OOP/StandardInterface/ArrayAccessTest.php
@@ -2,20 +2,33 @@
 
 class ArrayAccessTest extends \PHPUnit_Framework_TestCase
 {
-    public function testOffsetExists()
+    public function testOffsetExistsIsset()
     {
+        $mock = $this->getInstanceMock();
 
+        $mock->expects($this->once())->method('offsetExists')->with('key');
+
+        isset($mock['key']);
+    }
+
+    public function testIssetOffsetExists()
+    {
+        $mock = $this->getInstanceMock();
+
+        $mock->expects($this->never())->method('offsetGet');
+        $mock->expects($this->never())->method('offsetExists');
+
+        array_key_exists('key', $mock);
     }
 
     public function testOffsetGet()
     {
-        /* @var $mock ArrayAccess|PHPUnit_Framework_MockObject_MockObject */
-        $mock = $this->getMockBuilder('ArrayAccess')->getMock();
+        $mock = $this->getInstanceMock();
 
         $mock->expects($this->once())->method('offsetGet');
         $mock->expects($this->never())->method('offsetExists');
 
-        $mock[1];
+        $mock['key'];
     }
 
     public function testOffsetSet()
@@ -25,6 +38,18 @@ class ArrayAccessTest extends \PHPUnit_Framework_TestCase
 
     public function testOffsetUnset()
     {
+        $mock = $this->getInstanceMock();
 
+        $mock->expects($this->once())->method('offsetUnset')->with('key');
+
+        unset($mock['key']);
+    }
+
+    /**
+     * @return ArrayAccess|PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getInstanceMock()
+    {
+        return $this->getMockBuilder('ArrayAccess')->getMock();
     }
 }
