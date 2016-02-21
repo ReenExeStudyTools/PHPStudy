73ab6cd61b90febd46d41d0349064da6093f388f
ReenExe <ipourri@gmail.com>
2016-02-21 17:05:34 +0200

    start test `ArrayAccess`

---
+++ tests/OOP/StandardInterface/ArrayAccessTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class ArrayAccessTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        /* @var $mock ArrayAccess|PHPUnit_Framework_MockObject_MockObject */
+        $mock = $this->getMockBuilder('ArrayAccess')->getMock();
+
+        $mock->expects($this->once())->method('offsetGet');
+
+        $mock[1];
+    }
+}
