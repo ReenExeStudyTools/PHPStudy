7648d577c6c3f6de4bcc646a2b10b2aebb0b8b17
ReenExe <ipourri@gmail.com>
2016-03-19 19:46:26 +0200

    progress

--- tests/String/Function/SubStrCompareTest.php
+++ tests/String/Function/SubStrCompareTest.php
@@ -30,4 +30,30 @@ class SubStrCompareTest extends \PHPUnit_Framework_TestCase
             0
         ];
     }
+
+    /**
+     * @dataProvider dataProvider
+     * @param $mainString
+     * @param $string
+     * @param $offset
+     * @param $length
+     * @param $caseInsensitivity
+     * @param $expected
+     */
+    public function test($mainString, $string, $offset, $length, $caseInsensitivity, $expected)
+    {
+        $this->assertSame($expected, substr_compare($mainString, $string, $offset, $length, $caseInsensitivity));
+    }
+
+    public function dataProvider()
+    {
+        yield [
+            'abc',
+            'abc',
+            0,
+            3,
+            null,
+            0,
+        ];
+    }
 }
