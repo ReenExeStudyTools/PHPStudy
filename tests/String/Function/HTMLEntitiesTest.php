692aa4751d8f94aa7855ad2fc899e07bbe7ee504
ReenExe <ipourri@gmail.com>
2016-03-19 17:52:26 +0200

    use `$quoteStyle`

--- tests/String/Function/HTMLEntitiesTest.php
+++ tests/String/Function/HTMLEntitiesTest.php
@@ -4,13 +4,13 @@ class HTMLEntitiesTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider dataProvider
-     * @param $expected
      * @param $string
+     * @param $expected
      * @param null $quoteStyle
      * @param null $charset
      * @param null $doubleEncode
      */
-    public function test($expected, $string, $quoteStyle = null, $charset = null, $doubleEncode = null)
+    public function test($string, $expected, $quoteStyle = null, $charset = null, $doubleEncode = null)
     {
         $this->assertSame($expected, htmlentities($string, $quoteStyle, $charset, $doubleEncode));
     }
@@ -18,8 +18,19 @@ class HTMLEntitiesTest extends \PHPUnit_Framework_TestCase
     public function dataProvider()
     {
         yield [
-            '&lt;b&gt;Title&lt;/b&gt;',
             '<b>Title</b>',
+            '&lt;b&gt;Title&lt;/b&gt;',
+        ];
+
+        yield [
+            "<b>Name</b> 'Reen'",
+            "&lt;b&gt;Name&lt;/b&gt; 'Reen'",
+        ];
+
+        yield [
+            "<b>Name</b> 'Reen'",
+            "&lt;b&gt;Name&lt;/b&gt; &#039;Reen&#039;",
+            ENT_QUOTES
         ];
     }
 }
