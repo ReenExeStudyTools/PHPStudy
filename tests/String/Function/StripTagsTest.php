f026ad23dd8a3525877fa9c09a8fe2086c8d2094
ReenExe <ipourri@gmail.com>
2016-03-19 18:29:23 +0200

    progress

--- tests/String/Function/StripTagsTest.php
+++ tests/String/Function/StripTagsTest.php
@@ -29,4 +29,69 @@ class StripTagsTest extends \PHPUnit_Framework_TestCase
             ' value '
         ];
     }
+
+    /**
+     * @dataProvider dataProvider
+     * @param $string
+     * @param $expected
+     * @param $allowableTags
+     */
+    public function test($string, $expected, $allowableTags)
+    {
+        $this->assertSame($expected, strip_tags($string, $allowableTags));
+    }
+
+    public function dataProvider()
+    {
+        yield [
+            'text',
+            'text',
+            null,
+        ];
+
+        yield [
+            '<b>text</b>',
+            'text',
+            null,
+        ];
+
+        yield [
+            '<b>text</b>',
+            'text',
+            '<span></span>',
+        ];
+
+        yield [
+            '<b>text</b>',
+            '<b>text</b>',
+            '<b></b>',
+        ];
+
+        // only start tag - but stay closed too
+        yield [
+            '<b>text</b>',
+            '<b>text</b>',
+            '<b>',
+        ];
+
+        yield [
+            '<span><b>text</b></span>',
+            '<b>text</b>',
+            '<b></b>',
+        ];
+
+        // broken
+        yield [
+            '<b>text<b>',
+            '<b>text<b>',
+            '<b></b>',
+        ];
+
+        // broken
+        yield [
+            '<b>text',
+            '<b>text',
+            '<b></b>',
+        ];
+    }
 }
