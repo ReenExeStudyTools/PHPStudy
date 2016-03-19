ed326f378bdc0cdfb3cb0114d30fbeb04cc4787a
ReenExe <ipourri@gmail.com>
2016-03-19 19:28:07 +0200

    progress

--- tests/String/Function/WordCountTest.php
+++ tests/String/Function/WordCountTest.php
@@ -4,10 +4,10 @@ class WordCountTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider dataProvider
-     * @param $expected
      * @param $string
+     * @param $expected
      */
-    public function testDefault($expected, $string)
+    public function testDefault($string, $expected)
     {
         $this->assertSame($expected, str_word_count($string));
     }
@@ -15,8 +15,33 @@ class WordCountTest extends \PHPUnit_Framework_TestCase
     public function dataProvider()
     {
         yield [
+            '',
             0,
-            ''
+        ];
+
+        yield [
+            'This is some simple message',
+            5,
+        ];
+
+        yield [
+            'double with double',
+            3,
+        ];
+
+        yield [
+            'have some, comma',
+            3,
+        ];
+
+        yield [
+            "We're have quote",
+            3,
+        ];
+
+        yield [
+            "Digit between letter one2one",
+            5,
         ];
     }
 }
