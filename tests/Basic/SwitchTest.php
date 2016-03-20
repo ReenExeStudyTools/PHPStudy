3fcc5553e83974f475f9d6a0b62639f6dba24dfd
ReenExe <ipourri@gmail.com>
2016-03-20 18:41:10 +0200

    progress

--- tests/Basic/SwitchTest.php
+++ tests/Basic/SwitchTest.php
@@ -6,22 +6,25 @@ class SwitchTest extends \PHPUnit_Framework_TestCase
     {
         $callable = function ($value) {
             switch ($value) {
-                case 1: return 1;
+                case 1: return 'first';
             }
         };
 
-        $this->assertSame($callable(1), 1);
+        $this->assertSame($callable(1), 'first');
+        $this->assertSame($callable(2), null);
 
         $callable = function ($value) {
             switch ($value) {
-                case 1: return 1;
-                case '1': return '1';
+                case 1: return 'first';
+                case '1': return 'second';
+                case '2': return 'third';
             }
         };
 
-        $this->assertSame($callable(1), 1);
-        $this->assertSame($callable('1'), 1);
-        $this->assertSame($callable(true), 1);
+        $this->assertSame($callable(1), 'first');
+        $this->assertSame($callable('1'), 'first');
+        $this->assertSame($callable(true), 'first');
+        $this->assertSame($callable(2), 'third');
     }
 
     public function testArrayArgument()
@@ -33,5 +36,18 @@ class SwitchTest extends \PHPUnit_Framework_TestCase
         };
 
         $this->assertSame($callable([1]), [true]);
+    }
+
+    public function testSubCondition()
+    {
+        $callable = function ($value) {
+            switch (true) {
+                case in_array($value, [1, 2, 3]): return 'first';
+                case in_array($value, [5, 6, 7]): return 'second';
+            }
+        };
+
+        $this->assertSame($callable(1), 'first');
+        $this->assertSame($callable(7), 'second');
     }
 }
