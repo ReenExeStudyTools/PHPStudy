8dc5b6bfe1e300c93c6dff2ad924d47b898ddb4c
ReenExe <ipourri@gmail.com>
2016-04-13 17:11:32 +0300

    complete

--- tests/Basic/TernaryOperatorTest.php
+++ tests/Basic/TernaryOperatorTest.php
@@ -8,4 +8,23 @@ class TernaryOperatorTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(2 < 3 && 3 < 5 ? 'a': 'b', 'a');
         $this->assertSame((2 < 3 && 3 < 5) ? 'a': 'b', 'a');
     }
+
+    public function testShort()
+    {
+        $this->assertSame('a' ?: 'b', 'a');
+        $this->assertSame('' ?: 'b', 'b');
+    }
+
+    public function testSequence()
+    {
+        $this->assertSame('a' ? 'a': 'b' ? 'b' : 'c', 'b');
+        $this->assertSame('a' ? 'a': ('b' ? 'b' : 'c'), 'a');
+    }
+
+    public function testShortSequence()
+    {
+        $this->assertSame('a' ?: 'b' ?: 'c', 'a');
+        $this->assertSame('' ?: 'b' ?: 'c', 'b');
+        $this->assertSame('' ?: '' ?: 'c', 'c');
+    }
 }
