f22fbe025fe6e22b725976379fa8f3e826ada263
ReenExe <ipourri@gmail.com>
2016-04-21 22:02:20 +0300

    progress

--- tests/SPL/SplObjectStorageTest.php
+++ tests/SPL/SplObjectStorageTest.php
@@ -5,22 +5,23 @@ class SplObjectStorageTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider containsDataProvider
      * @param $value
+     * @param $message
      */
-    public function testContains($value)
+    public function testContains($value, $message)
     {
         $obj = new \SplObjectStorage();
 
-        $this->setExpectedException('PHPUnit_Framework_Error_Warning');
+        $this->setExpectedException('PHPUnit_Framework_Error_Warning', $message);
 
         $obj->contains($value);
     }
 
     public function containsDataProvider()
     {
-        yield [true];
-        yield [false];
-        yield [1];
-        yield ['1'];
-        yield [[]];
+        yield [true,    'SplObjectStorage::contains() expects parameter 1 to be object, boolean given'];
+        yield [false,   'SplObjectStorage::contains() expects parameter 1 to be object, boolean given'];
+        yield [1,       'SplObjectStorage::contains() expects parameter 1 to be object, integer given'];
+        yield ['1',     'SplObjectStorage::contains() expects parameter 1 to be object, string given'];
+        yield [[],      'SplObjectStorage::contains() expects parameter 1 to be object, array given'];
     }
 }
